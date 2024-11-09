c8b297cbc3bee706746d9c67eb8c8bd1a8f2f5d7
MC <88674101+MandarChitale123@users.noreply.github.com>
2024-11-08 21:37:56 -0500

    Update CommandSwerveDrivetrain.java

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModule;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 
 public class CommandSwerveDrivetrain extends SwerveDriveTrain{
   
