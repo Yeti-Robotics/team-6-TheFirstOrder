bdf416fc8a872e83594589574cc4213533e66387
MC <88674101+MandarChitale123@users.noreply.github.com>
2024-11-17 17:29:44 -0500

    Update IntakeSubsystem.java

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj.DoubleSolenoid
 
 public class IntakeSubsystem extends SubsystemBase {
     private final TalonFX intakeKraken;
