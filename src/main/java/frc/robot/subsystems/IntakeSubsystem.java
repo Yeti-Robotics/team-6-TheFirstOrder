475d7b04f56169d8c11784fbef95d79022ca9c67
Ankit Panda <124918387+Ankit-Panda-2010@users.noreply.github.com>
2024-11-17 21:46:23 -0500

    Update IntakeSubsystem.java

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,6 +1,6 @@
 package frc.robot.subsystems;
 
-//Imports from WPI Lib
+//Imports
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
@@ -11,12 +11,12 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-
+//Subsystem Class
 public class IntakeSubsystem extends SubsystemBase {
     private final TalonFX intakeKraken;
     private final DigitalInput beamBreak;
     public final Trigger intakeOccupiedTrigger;
-
+//Constants Class
     public class IntakeConstants {
         public static final int INTAKE_KRAKEN_ID = 6;
         public static final InvertedValue INTAKE_INVERSION = InvertedValue.Clockwise_Positive;
@@ -36,7 +36,6 @@ public class IntakeSubsystem extends SubsystemBase {
         intakeOccupiedTrigger = new Trigger(this::getBeamBreak);
         configs.MotorOutput.Inverted = IntakeConstants.INTAKE_INVERSION;
         configs.MotorOutput.NeutralMode = IntakeConstants.INTAKE_NEUTRAL_MODE;
-        //configs.FutureProofConfigs = Constants.TalonFXConstants.TALON_FUTURE_PROOF;
         intakeKraken.getRotorVelocity().waitForUpdate(IntakeConstants.INTAKE_VELOCITY_STATUS_FRAME);
         intakeKraken.getRotorPosition().waitForUpdate(IntakeConstants.INTAKE_POSITION_STATUS_FRAME);
         intakeConfigurator.apply(configs);
